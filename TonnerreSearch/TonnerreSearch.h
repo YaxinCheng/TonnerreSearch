e5d78129b204cd98af19c7fe59ee9a8b4ce07389
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-04-22 13:39:52 -0400

    Initial Commit

---
+++ TonnerreSearch/TonnerreSearch.h
@@ -0,0 +1,19 @@
+//
+//  TonnerreSearch.h
+//  TonnerreSearch
+//
+//  Created by Yaxin Cheng on 2018-04-22.
+//  Copyright © 2018 Yaxin Cheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TonnerreSearch.
+FOUNDATION_EXPORT double TonnerreSearchVersionNumber;
+
+//! Project version string for TonnerreSearch.
+FOUNDATION_EXPORT const unsigned char TonnerreSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TonnerreSearch/PublicHeader.h>
+
+
